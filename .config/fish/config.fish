70a891d809b81ed2db7a925df4ddd84d5365f893
devkade <mouseku43@gmail.com>
2024-09-11 19:10:24 +0900

    paste my setting

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,6 +15,16 @@ alias la "ls -A"
 alias ll "ls -l"
 alias lla "ll -A"
 alias g git
+alias v nvim
+alias h "cd $HOME"
+alias c clear
+alias l "eza -lah"
+alias vcf "cd /Users/kade/.config/nvim/lua"
+alias cf "cd /Users/kade/.config/"
+alias fcf "cd /Users/kade/.config/fish"
+alias frc "/Users/kade/.config/fish/config.fish"
+alias ide "~/.config/tmux/ide.fish"
+alias moana="ssh mouseku@moana.khu.ac.kr -p 30080"
 command -qv nvim && alias vim nvim
 
 set -gx EDITOR nvim
@@ -43,3 +53,16 @@ set LOCAL_CONFIG (dirname (status --current-filename))/config-local.fish
 if test -f $LOCAL_CONFIG
     source $LOCAL_CONFIG
 end
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/anaconda3/bin/conda
+    eval /opt/anaconda3/bin/conda "shell.fish" hook $argv | source
+else
+    if test -f "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+        . "/opt/anaconda3/etc/fish/conf.d/conda.fish"
+    else
+        set -x PATH /opt/anaconda3/bin $PATH
+    end
+end
+# <<< conda initialize <<<
